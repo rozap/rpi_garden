d3f88b98609aaf99f3c994c7b3c2d1f0d7ddda92
chris duranti <chrisd1891@gmail.com>
2014-05-29 19:51:53 -0700

    stuff

--- level.py
+++ level.py
@@ -15,7 +15,7 @@ def main():
         try:
             bus.write_byte_data(0x70, 0, 81)
             sleep(.02)
-            bytes = bus.read_i2c_block_data(DEVICE_ADDRESS, 1)
+            bytes = i2c.read_word_data(0x70, 2) / 255
             print bytes
             sleep(1)
         except IOError:
