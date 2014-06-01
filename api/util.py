43f942848b6fc148d6f6e3a2a610bcdc98fed444
chris <chrisd1891@gmail.com>
2014-05-31 20:09:05 -0700

    stuff :sparkles:

--- api/util.py
+++ api/util.py
@@ -13,7 +13,6 @@ class Encoder(json.JSONEncoder):
 def json_view(fn):
     def wrapped(*args, **kwargs):
         res = fn(*args, **kwargs)
-        print res
         if type(res) == list or len(res) == 1 or type(res) == dict:
             result = res
             status = 200
